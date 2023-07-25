a5e98d076f847c3ffa0f9a4a57241564f6c92af6
Michel Marinho <michel.marinho04@outlook.com>
2023-07-25 13:28:17 -0400

    feat: docker compose

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,3 @@
-import "@fontsource/roboto/300.css";
-import "@fontsource/roboto/400.css";
-import "@fontsource/roboto/500.css";
-import "@fontsource/roboto/700.css";
-
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { HelmetProvider } from "react-helmet-async";
